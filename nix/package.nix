3af78f4d35e67fcde64616c6150d39250f37e5de
Beaudan Brown <beaudan.brown@gmail.com>
2026-05-16 15:22:44 +1000

    Copy extension runtime dependencies

--- nix/package.nix
+++ nix/package.nix
@@ -54,7 +54,7 @@ JSON
     ''}
 
     mkdir -p "$out/share/pi-harness/agent/extensions/node_modules"
-    ln -s "${piPackage}/lib/node_modules/@earendil-works/pi-coding-agent/node_modules/typebox" \
+    cp -R "${piPackage}/lib/node_modules/@earendil-works/pi-coding-agent/node_modules/typebox" \
       "$out/share/pi-harness/agent/extensions/node_modules/typebox"
 
     mkdir -p "$out/bin"
